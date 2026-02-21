e6088b5b82d79be8f886ad47ad7275197558acd2
dttjader <daniel@tjader.com.br>
2026-02-21 10:34:53 -0300

    v2 - commit2

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FootballTeamBuilder from './FootballTeamBuilder.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FootballTeamBuilder />
+  </React.StrictMode>,
+)
